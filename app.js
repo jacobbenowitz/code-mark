f91a9d822fda734e9179d34d186725e6e24531f4
Run Qiang Mei <run.qiang.mei.1999@gmail.com>
2022-05-08 18:43:28 -0400

    did backend for user auth

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
+const mongoose = require('mongoose');
 const express = require("express");
+const bodyParser = require('body-parser');
+const users = require("./routes/api/users");
+const passport = require('passport');
+
 const app = express();
+const db = require('./config/keys').mongoURI;
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World"));
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 
