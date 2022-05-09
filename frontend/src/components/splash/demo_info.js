9c36b4fc77a33f01c647e57fecf0b3448056409d
Chris <moorecash@gmail.com>
2022-05-09 15:11:45 -0400

    3pm meeting

--- frontend/src/components/splash/demo_info.js
+++ frontend/src/components/splash/demo_info.js
@@ -1,69 +1,80 @@
-// import React from "react";
+import React from "react";
 
-// export const DemoInfo = (
-//     <div id="landing-info">
-//         <div id="demo-title">
-//             <h3>The home of ideation</h3>
-//         </div>
+export const DemoInfo = (
+    <div id="landing-info">
+        <div id="demo-title">
+            <h3>Learn and grow your code!</h3>
+        </div>
 
-//         <div id="demo-info">
-//             <div id="info-1" className="info-box">
-//                 <div className="icon-container">
-//                     <img src="https://my5000px-static.s3.amazonaws.com/landing_page_icons/grow.svg" />
-//                 </div>
-//                 <div className="info-box-text">
-//                     <div className="info-sub-header">
-//                         <h4>Grow as a photographer</h4>
-//                     </div>
-//                     <div className="info-desc-box">
-//                         <p>Get immediate exposure with your first upload. Our Pulse algorithm surfaces new photographs and photographers, ensuring your photos are seen by the community so you receive valuable feedback on day one.</p>
-//                     </div>
-//                 </div>
-//             </div>
+        <div id="demo-info">
+            <div id="info-1" className="info-box">
+                <div className="icon-container">
+                    <img src="https://my5000px-static.s3.amazonaws.com/landing_page_icons/grow.svg" />
+                </div>
+                <div className="info-box-text">
+                    <div className="info-sub-header">
+                        <h4>Grow as a coder</h4>
+                    </div>
+                    <div className="info-desc-box">
+                        <p>Get immediate feedback with your note save. 
+                            Our internet research algorithm find valuable 
+                            resources online so you can reinforce the code you're
+                            learning with additional documentation.</p>
+                    </div>
+                </div>
+            </div>
 
-//             <div id="info-2" className="info-box">
-//                 <div className="icon-container">
-//                     <img src="https://my5000px-static.s3.amazonaws.com/landing_page_icons/career.svg" />
-//                 </div>
-//                 <div className="info-box-text">
-//                     <div className="info-sub-header">
-//                         <h4>Build your career</h4>
-//                     </div>
-//                     <div className="info-desc-box">
-//                         <p>Market yourself as a professional photographer. Show that you’re available for hire on your Profile and get discovered.</p>
-//                     </div>
-//                 </div>
-//             </div>
+            <div id="info-2" className="info-box">
+                <div className="icon-container">
+                    <img src="https://my5000px-static.s3.amazonaws.com/landing_page_icons/career.svg" />
+                </div>
+                <div className="info-box-text">
+                    <div className="info-sub-header">
+                        <h4>Build your codebase</h4>
+                    </div>
+                    <div className="info-desc-box">
+                        <p>Save invaluable time by having all your saved notes 
+                            and code documen at your fingertips!
+                        </p>
+                    </div>
+                </div>
+            </div>
 
-//             <div id="info-3" className="info-box">
-//                 <div className="icon-container">
-//                     <img src="https://my5000px-static.s3.amazonaws.com/landing_page_icons/performance.svg" />
-//                 </div>
-//                 <div className="info-box-text">
-//                     <div className="info-sub-header">
-//                         <h4>See how you're performing</h4>
-//                     </div>
-//                     <div className="info-desc-box">
-//                         <p>With Statistics and Pulse you get valuable insights into how your photos are performing and how you rank in comparison to other photographers in the community.</p>
-//                     </div>
-//                 </div>
-//             </div>
+            <div id="info-3" className="info-box">
+                <div className="icon-container">
+                    <img src="https://my5000px-static.s3.amazonaws.com/landing_page_icons/performance.svg" />
+                </div>
+                <div className="info-box-text">
+                    <div className="info-sub-header">
+                        <h4>Join the Community</h4>
+                    </div>
+                    <div className="info-desc-box">
+                        <p>With the ability to share notes, and comment on others'
+                            pages, you're joining a community of active learners.
+                        </p>
+                    </div>
+                </div>
+            </div>
 
-//             <div id="info-4" className="info-box">
-//                 <div className="icon-container">
-//                     <img src="https://my5000px-static.s3.amazonaws.com/landing_page_icons/money.svg"></img>
-//                 </div>
-//                 <div className="info-box-text">
-//                     <div className="info-sub-header">
-//                         <h4>Sell your work</h4>
-//                     </div>
-//                     <div className="info-desc-box">
-//                         <p>Earn one of the highest royalty rates in the industry when you distribute your photography through 500px to a global marketplace, where buyers can view and purchase your work for commercial usage.</p>
-//                     </div>
-//                 </div>
-//             </div>
+            <div id="info-4" className="info-box">
+                <div className="icon-container">
+                    <img src="https://my5000px-static.s3.amazonaws.com/landing_page_icons/money.svg"></img>
+                </div>
+                <div className="info-box-text">
+                    <div className="info-sub-header">
+                        <h4>Improve faster</h4>
+                    </div>
+                    <div className="info-desc-box">
+                        <p>By saving your notes, and being able to peruse the 
+                            notes and comments of others, you're laying down 
+                            neurons in the fastest way possible, propelling 
+                            your codebase and knowledge exponentially!
+                        </p>
+                    </div>
+                </div>
+            </div>
 
-//         </div>
+        </div>
 
-//     </div>
-// )
+    </div>
+)
--- frontend/src/components/splash/splash_page.js
+++ frontend/src/components/splash/splash_page.js
@@ -1,35 +1,18 @@
-// import React from "react";
-// import Link from "react-router-dom";
-// import { Hero } from "./hero";
-// import { DemoInfo } from "./demo_info";
-// import SimpleCTA from "./simple_cta_section";
+import React from "react";
+import { Hero } from "./hero";
+import { DemoInfo } from "./demo_info";
 
-// export default class SplashPage extends React.Component {
 
-//   render() {
-//     return (
-//        <div id="landing-page">
-//       { Hero }
-//       { DemoInfo }
+export default class SplashPage extends React.Component {
 
-//       <SimpleCTA key={'cta-landing'} props={props} />
-//     </div>
-
-//       // <div className="splash-container">
-//       //   {/* hero */}
-//       //   <div className="splash-hero">
-//       //     <div className="hero-left-col">
-//       //       <h1>Welcome to CodeMark</h1>
-//       //       <p className="tagline">Smart bookmarks for code</p>
-//       //     </div>
-//       //     <div className="hero-right-col">
-//       //       {/* login and signup buttons */}
-//       //     </div>
-//       //   </div>
-
-//       //   {/* summary of features */}
-//       //   {/* credits to team members */}
-//       // </div>
-//     )
-//   }
-// }
+  render() {
+    return (
+      <div id="landing-page">
+        { Hero }
+        {/* summary of features */}
+        {DemoInfo}
+        {/* credits to team members */}
+      </div>
+    )
+  }
+}
