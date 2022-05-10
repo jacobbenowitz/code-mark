5b41a719389431e7eec0534ab265205bfbbbfd0a
Jacob Benowitz <jacob.benowitz@gmail.com>
2022-05-10 17:45:16 -0400

    edit form modal complete

--- frontend/src/components/main/note_show.js
+++ frontend/src/components/main/note_show.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CodeEditorReadOnly from '../code_editor/code_editor_readonly';
 import NoteShowEditorLoader from '../code_editor/code_show_editor_loader';
+import EditNote from '../code_editor/edit_note'
 
 export default class NoteShow extends React.Component {
   constructor(props) {
@@ -28,57 +29,81 @@ export default class NoteShow extends React.Component {
     })
   }
 
+  toggleEditModal() {
+    const editNoteModal = document.getElementById('edit-note-container');
+    if (editNoteModal.className = "modal-off") {
+      editNoteModal.className = "modal-on"
+    } else {
+      editNoteModal.className = "modal-off"
+    }
+  }
+
   render() {
-    const { note } = this.props;
+    const { note, currentUser, updateNote } = this.props;
     return note ? (
-      <div className='note-show-container center-span-7'>
-        <div className='note-show-top-icons'>
-          <div className='back-page icon-button'
-            onClick={() => this.props.history.push('/home')}
-          >
-            <i className="fa-solid fa-arrow-left fa-lg"></i>
-            <span>
-              Back
-            </span>
-          </div>
-          <div className='delete-note icon-button' onClick={this.deleteNote}>
-            <i className="fa-solid fa-trash fa-lg"></i>
-            <span>
-              Delete
-            </span>
+      <>
+        <div id='edit-note-container' className="modal-off">
+          <div className='edit-wrapper'>
+            <EditNote note={note} updateNote={updateNote}
+              currentUser={currentUser} />
           </div>
         </div>
-
-        <div className='note-show-main'>
-          <div className='note-show-title'>
-            <h1>{note.title}</h1>
+        <div className='note-show-container center-span-7'>
+          <div className='note-show-top-icons'>
+            <div className='back-page icon-button'
+              onClick={() => this.props.history.push('/home')}>
+              <i className="fa-solid fa-arrow-left fa-lg"></i>
+              <span>
+                Back
+              </span>
+            </div>
+            <div className='edit-note icon-button'
+              onClick={() => this.toggleEditModal()}>
+              <i class="fa-solid fa-pen-to-square fa-lg"></i>
+              <span>
+                Edit
+              </span>
+            </div>
+            <div className='delete-note icon-button'
+              onClick={() => this.deleteNote()}>
+              <i className="fa-solid fa-trash fa-lg"></i>
+              <span>
+                Delete
+              </span>
+            </div>
           </div>
 
-          <div className='code-note-body'>
-            <CodeEditorReadOnly
-              codeBody={note.codebody}
-            />
+          <div className='note-show-main'>
+            <div className='note-show-title'>
+              <h1>{note.title}</h1>
+            </div>
+
+            <div className='code-note-body'>
+              <CodeEditorReadOnly
+                codeBody={note.codebody}
+              />
+            </div>
+
+            <div className='note-textDetails'>
+              <span className='textDetails-show'>
+                {note.textdetails}
+              </span>
+            </div>
           </div>
 
-          <div className='note-textDetails'>
-            <span className='textDetails-show'>
-              {note.textdetails}
-            </span>
+          <div className='note-resources'>
+            <div className='resources-title'>
+              <h4>Resources</h4>
+            </div>
+            <div className='resources-list'>
+              {/* resourceItem components */}
+            </div>
           </div>
         </div>
-
-        <div className='note-resources'>
-          <div className='resources-title'>
-            <h4>Resources</h4>
-          </div>
-          <div className='resources-list'>
-            {/* resourceItem components */}
-          </div>
-        </div>
-      </div>
-    ) : (
-      <NoteShowEditorLoader />
-    )
+      </>
+        ) : (
+        <NoteShowEditorLoader />
+        )
 
   }
 }
--- frontend/src/components/main/note_show_container.js
+++ frontend/src/components/main/note_show_container.js
@@ -1,18 +1,20 @@
 import { connect } from "react-redux";
 import NoteShow from "./note_show";
-import { fetchNote, removeNote } from "../../actions/note_actions";
+import { fetchNote, removeNote, updateNote } from "../../actions/note_actions";
 
 const mapStateToProps = (state, { match }) => {
   return {
     noteId: match.params.noteId,
-    note: state.notes.all[match.params.noteId]
+    note: state.notes.all[match.params.noteId],
+    currentUser: state.session.user
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
     fetchNote: noteId => dispatch(fetchNote(noteId)),
-    removeNote: noteId => dispatch(removeNote(noteId))
+    removeNote: noteId => dispatch(removeNote(noteId)),
+    updateNote: noteData => dispatch(updateNote(noteData))
   }
 }
 
