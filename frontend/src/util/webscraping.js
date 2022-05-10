17532863f768a6c177f6a2740086daf64cdf5008
Run Qiang Mei <run.qiang.mei.1999@gmail.com>
2022-05-10 01:31:20 -0400

    did some webscrapping

--- frontend/src/util/webscraping.js
+++ frontend/src/util/webscraping.js
@@ -1,47 +1,66 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const Nightmare = require('nightmare');
+const fs = require('fs');
 
-axios
-    .get('https://developer.mozilla.org/en-US/docs/Web/CSS#key_resources')
-    .then(response => {
-        let $ = cheerio.load(response.data);
-        $('dd').each((_idx,element) => console.log($(element).text().trim()))
-    })
-    .catch(error => console.log(error));
+// axios
+//     .get('https://developer.mozilla.org/en-US/docs/Web/CSS#key_resources')
+//     .then(response => {
+//         let $ = cheerio.load(response.data);
+//         $('dd').each((_idx,element) => console.log($(element).text().trim()))
+//     })
+//     .catch(error => console.log(error));
 
-const getAdvice = (keywords) => {
-    var advice = [];
+const getAdvice = (keyword) => {
+    // var advice = [];
     const nightmare = Nightmare({ show: true });
-    keywords.forEach(keyword => {
-        nightmare
-            .goto('https://developer.mozilla.org/en-US/')
-            .type('#hp-search-q',keyword)
-            .click('.button.action.has-icon.search-button')
-            .wait('ul.search-results-list li:first-child p.search-result-url a')
-            .evaluate(() => document.querySelector('ul.search-results-list li:first-child p.search-result-url a').href)
-            .end()
-            .then(response => advice.push(response))
-            .catch(err => console.log('search failed:',err))
-    })
-    return advice;
+    nightmare
+        .goto('https://developer.mozilla.org/en-US/')
+        .wait('#hp-search-q')
+        .type('#hp-search-q',keyword)
+        .click('#hp-search-form > button.button.action.has-icon.search-button')
+        .wait('#content > article > div.search-results > ul > li:nth-child(1) > p.search-result-url > a')
+        // .evaluate(() => document.querySelector('ul.search-results-list li:first-child p.search-result-url a').href)
+        .evaluate(() => document.querySelector('#content > article > div.search-results > ul > li:nth-child(1) > p.search-result-url > a').href)
+        .end()
+        // .then(response => advice.push(response))
+        .then(console.log)
+        .catch(err => console.log('search failed:',err))
+    // return advice;
 }
 
-const getAdvice2 = async (keywords) => {
+const getAdvice2 = async (keyword) => {
     try {
-        const advice = [];
-        keywords.forEach(async (keyword) => {
+        // keywords.forEach(async (keyword) => {
             const {data} = await axios.get(`https://developer.mozilla.org/en-US/search?q=${keyword.split(' ').join('+')}`);
             const $ = cheerio.load(data);
-            advice.push($('ul.search-results-list li:first-child p.search-result-url a'))
-        })
+            const advice = [];
+            // console.log($('.search-result-url > a'));
+            // $('.search-result-url > a').each((_idx,element) => {
+            console.log(`https://developer.mozilla.org/en-US/search?q=${keyword.split(' ').join('+')}`)
+            console.log($('a').length)
+            $('a').each((_idx,element) => {
+                console.log($(element).text())
+                advice.push($(element).text())
+            });
+            // $('.search-result-url').each((_idx,element) => advice.push($(element).text()));
+            // console.log(help);
+            // if (help.length !== 0){
+            //     // advice.push(help)
+            // }
+        // })
         return advice;
     } catch (error) {
         throw error;
     }
 };
 
-// export default getAdvice;
+// // export default getAdvice;
+
+// getAdvice2('ruby')
+//     .then(data => console.log(data));
+
+getAdvice('ruby');
 
 
 // const getPostTitles = async () => {
