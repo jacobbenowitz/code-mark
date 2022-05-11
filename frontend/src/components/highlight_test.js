71bcd42a1b9d91760d610b6b2671636b9bbf2342
Run Qiang Mei <run.qiang.mei.1999@gmail.com>
2022-05-11 16:04:04 -0400

    did work on highlight

--- frontend/src/components/highlight_test.js
+++ frontend/src/components/highlight_test.js
@@ -1,4 +1,5 @@
 import React from "react";
+let hljs = require('highlight.js');
 
 export default class HighlightTest extends React.Component {
     render(){
@@ -6,11 +7,21 @@ export default class HighlightTest extends React.Component {
             <div>
                 <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/styles/default.min.css"></link>
                 <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/highlight.min.js"></script>
-                <script>hljs.highlightAll();</script>
-                <pre><code class="language-html">
+                <script>
+                    hljs.highlightAll();
+                    html = hljs.highlightAuto('<h1>Hello World!</h1>').value
+                </script>
+                <script>
                     document.addEventListener('DOMContentLoaded', (event) => {
                         document.querySelectorAll('pre code').forEach((el) => {
-                            // hljs.highlightElement(el)
+                            hljs.highlightElement(el)
+                        })
+                    })
+                </script>
+                <pre><code className="javascript">
+                    document.addEventListener('DOMContentLoaded', (event) => {
+                        document.querySelectorAll('pre code').forEach((el) => {
+                            hljs.highlightElement(el)
                         })
                     })
                 </code></pre>
