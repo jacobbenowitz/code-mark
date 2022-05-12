2c3aaf54067a55e4f8a1fb1585a715744f1d93ce
Chris <moorecash@gmail.com>
2022-05-11 21:34:20 -0400

    added dummy delete button

--- frontend/src/components/notes/comments/comment_item.js
+++ frontend/src/components/notes/comments/comment_item.js
@@ -2,11 +2,19 @@ import React from "react";
 
 const CommentItem = props => {
    
+
+  // deleteNote() {
+  //   this.props.removeNote(this.props.noteId).then(() => {
+  //     this.props.history.push('/home')
+  //   })
+  // }
+
   return props.comment ? (
     <div className="comment-wrapper">
       <div className="user-info-wrapper">
         <div className="avatar-wrapper">
           <div className="avatar-container-sm">
+            {/* need to access users to splice username letters */}
             {/* {props.comment.username} */}
             <span>CM</span>
           </div>
@@ -22,6 +30,13 @@ const CommentItem = props => {
       </div>
       <div className="comment-body-wrapper">
         <span className="comment-body">{props.comment.textbody}</span>
+      </div>
+      <div className='delete-note icon-button'
+        onClick={() => this.deleteNote()}>
+        <i className="fa-solid fa-trash fa-lg"></i>
+        <span>
+          Delete Comment
+        </span>
       </div>
     </div>
   ) : (<h4>Loading Comments</h4>)
