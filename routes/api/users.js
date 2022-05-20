dc7505829b2b9bb4378ef10a1a964c5154d56c2d
Jacob Benowitz <jacob.benowitz@gmail.com>
2022-05-20 13:59:33 -0400

    found update bug

--- routes/api/users.js
+++ routes/api/users.js
@@ -12,103 +12,103 @@ const Note = require("../../models/Note");
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
 router.get('/', (req, res) => {
-    User.find()
-        .then(users => res.json(users))
-        .catch(err => res.status(404).json({ nousersfound: 'No Users Found' }));
+  User.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(404).json({ nousersfound: 'No Users Found' }));
 })
 
 router.post('/register', (req, res) => {
-    const { errors, isValid } = validateRegisterInput(req.body);
+  const { errors, isValid } = validateRegisterInput(req.body);
 
-    if (!isValid) {
-        return res.status(400).json(errors);
-    }
-    User.findOne({ email: req.body.email })
-        .then(user => {
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        return res.status(400).json({ email: "A user has already registered with this address" })
+      } else {
+        User.findOne({ username: req.body.username })
+          .then(user => {
             if (user) {
-                return res.status(400).json({ email: "A user has already registered with this address" })
+              return res.status(400).json({ username: "A user has already registered with this name" })
             } else {
-                User.findOne({ username: req.body.username })
-                    .then(user => {
-                        if (user) {
-                            return res.status(400).json({ username: "A user has already registered with this name" })
-                        } else {
-                            const newUser = new User({
-                                username: req.body.username,
-                                email: req.body.email,
-                                password: req.body.password
-                            })
+              const newUser = new User({
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password
+              })
 
-                            bcrypt.genSalt(10, (err, salt) => {
-                                bcrypt.hash(newUser.password, salt, (err, hash) => {
-                                    if (err) throw err;
-                                    newUser.password = hash;
-                                    newUser.save()
-                                        .then(user => res.json(user))
-                                        .catch(err => console.log(err))
-                                })
-                            })
-                        }
-                    }
-                    )
+              bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                  if (err) throw err;
+                  newUser.password = hash;
+                  newUser.save()
+                    .then(user => res.json(user))
+                    .catch(err => console.log(err))
+                })
+              })
             }
-        })
+          }
+          )
+      }
+    })
 }
 )
 
 router.post('/login', (req, res) => {
-    const { errors, isValid } = validateLoginInput(req.body);
+  const { errors, isValid } = validateLoginInput(req.body);
 
-    if (!isValid) {
-        return res.status(400).json(errors);
-    }
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
 
-    const nameValue = req.body.usernameOrEmail;
-    const password = req.body.password;
+  const nameValue = req.body.usernameOrEmail;
+  const password = req.body.password;
 
-    User.findOne({ $or: [{ username: nameValue }, { email: nameValue }] })
-        .then(user => {
-            if (!user) {
-                return res.status(404).json({ username: 'This user does not exist' });
-            }
-            bcrypt.compare(password, user.password)
-                .then(isMatch => {
-                    if (isMatch) {
-                        const payload = { id: user.id, username: user.username };
+  User.findOne({ $or: [{ username: nameValue }, { email: nameValue }] })
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({ username: 'This user does not exist' });
+      }
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            const payload = { id: user.id, username: user.username };
 
-                        jwt.sign(
-                            payload,
-                            keys.secretOrKey,
-                            // Tell the key to expire in one hour
-                            { expiresIn: 3600 },
-                            (err, token) => {
-                                res.json({
-                                    success: true,
-                                    token: 'Bearer ' + token
-                                });
-                            }
-                        );
-                    } else {
-                        return res.status(400).json({ password: 'Incorrect password' });
-                    }
-                })
+            jwt.sign(
+              payload,
+              keys.secretOrKey,
+              // Tell the key to expire in one hour
+              { expiresIn: 3600 },
+              (err, token) => {
+                res.json({
+                  success: true,
+                  token: 'Bearer ' + token
+                });
+              }
+            );
+          } else {
+            return res.status(400).json({ password: 'Incorrect password' });
+          }
         })
+    })
 })
 
 router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
-    res.json({
-        id: req.user.id,
-        username: req.user.username,
-        email: req.user.email
-    });
+  res.json({
+    id: req.user.id,
+    username: req.user.username,
+    email: req.user.email
+  });
 })
 
 router.get('/:userId', (req, res) => {
-    User.findById(req.params.userId)
-        .then(user => res.json(user))
-        .catch(err =>
-            res.status(404).json({ nouserfound: "No User Found With That ID" })
-        );
+  User.findById(req.params.userId)
+    .then(user => res.json(user))
+    .catch(err =>
+      res.status(404).json({ nouserfound: "No User Found With That ID" })
+    );
 });
 
 router.patch('/:userId', passport.authenticate('jwt', { session: false }), (req, res) => {
@@ -151,7 +151,7 @@ router.patch('/:userId', passport.authenticate('jwt', { session: false }), (req,
             }
           })
       })
-      .catch (err => res.status(404).json(
+      .catch(err => res.status(404).json(
         { nouserfound: "No User Found With That ID" }
       ));
   }
