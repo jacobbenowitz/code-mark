ef650d591d3009914496b196a7c72d35d273524f
Jacob Benowitz <jacob.benowitz@gmail.com>
2022-05-23 18:10:05 -0400

    functionality of likeNoteIcon

--- frontend/src/components/notes/like_note_icon.js
+++ frontend/src/components/notes/like_note_icon.js
@@ -6,37 +6,66 @@ export default class LikeNoteIcon extends React.Component {
     this.state = {
       liked: false
     }
+    this.addLike = this.addLike.bind(this);
+    this.removeLike = this.removeLike.bind(this);
   }
 
   componentDidMount() {
-    this.props.likes.includes(this.props.currentUserId) ? (
-      this.setState({liked: true})
-    ) : (
-      this.setState({liked: false})
-    )
+    // this.props.likes.includes(this.props.currentUserId) ? (
+    //   this.setState({ liked: true })
+    // ) : (
+    //   this.setState({ liked: false })
+    // )
   }
 
   addLike() {
-    let newLikes = this.props.likes.push(this.props.currentUserId);
-    let data = { "likes": newLikes };
+    // let newLikes = this.props.likes.push(this.props.currentUserId);
+    // let data = { "likes": newLikes };
     this.setState(
-      { liked: true },
-      this.props.addNoteLike(data, this.props.noteId))
+      { liked: true })
+    // this.setState(
+    //   { liked: true },
+    //   this.props.addNoteLike(data, this.props.noteId))
   }
 
   removeLike() {
-    let filteredLikes = this.props.likes.filter(userId =>
-      userId !== this.props.currentUserId);
-    let data = { "likes": filteredLikes };
+    // let filteredLikes = this.props.likes.filter(userId =>
+    //   userId !== this.props.currentUserId);
+    // let data = { "likes": filteredLikes };
     this.setState(
-      { liked: false },
-      this.props.addNoteLike(data, this.props.noteId))
+      { liked: false })
+    // this.setState(
+    //   { liked: false },
+    //   this.props.addNoteLike(data, this.props.noteId))
+  }
+
+  getIcon() {
+    if (this.state.liked) {
+      return (
+        <div className='icon-wrapper'>
+          <i className="fa-solid fa-heart-circle-minus fa-xl" />
+        </div>
+      )
+    } else {
+      return (
+        <div className='icon-wrapper'>
+          <i className="fa-solid fa-heart-circle-plus fa-xl"
+          />
+        </div>
+      )
+    }
+  }
+
+  handleClick() {
+    this.state.liked ? (
+      this.setState({ liked: false })
+    ) : this.setState({ liked: true })
   }
 
   render() {
     return (
-      <div className='note-icon like'>
-        <i class="fa-solid fa-heart-circle-plus fa-xl"></i>
+      <div className='note-icon like' onClick={() => this.handleClick()}>
+        {this.getIcon()}
       </div>
     )
   }
