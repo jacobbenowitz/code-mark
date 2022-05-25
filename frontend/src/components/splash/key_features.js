3350e25e1c1d479af288a2934bbef08badf75c03
Chris <moorecash@gmail.com>
2022-05-25 15:36:01 -0400

    finish splash

--- frontend/src/components/splash/key_features.js
+++ frontend/src/components/splash/key_features.js
@@ -4,7 +4,7 @@ export const KeyFeatures = (
     <div id="features-container">
         <div id="features-left-container">
             <h4> Discover Notes </h4>
-            <p> Lorem Ipsum </p>
+            <p> Find the most useful code snippets. Our discover page is the best place to find new and valuable code from around the world. </p>
         </div>
         <div id="features-right-container">
             <img src="https://code-mark.s3.amazonaws.com/codemark-code-notes-grid.svg"></img>
